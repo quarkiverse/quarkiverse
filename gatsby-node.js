3ee24c89b820381ae852a36e0c9c7d8e4c5ad33a
Holly Cummins <hcummins@redhat.com>
2023-07-07 20:29:31 +0100

    Fix build breaks which only showed in build mode, not develop mode

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,11 +8,4 @@
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
 }
