c57a3c6e0fc6c4316572aa8c8412dfca70d6e357
Holly Cummins <hcummins@redhat.com>
2023-10-24 19:23:12 +0100

    Use plugin for header links

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
+                    `gatsby-remark-autolink-headers`,
                     `gatsby-remark-emoji`,
                 ]
             },
